d3f7c44fece3387e4c78ba1c9f6f088133d1cd53
Ahmed-W-Rabea <ahmedwael11425@gmail.com>
2024-08-08 19:13:52 +0300

    "finish home screen and start coding the bloc files"

---
+++ lib/bloc/weather_bloc_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
+
+part 'weather_bloc_event.dart';
+part 'weather_bloc_state.dart';
+
+class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
+  WeatherBlocBloc() : super(WeatherBlocInitial()) {
+    on<FetchWeather>((event, emit) async {
+      try {
+        Position position = await Geolocator.getCurrentPosition();
+        WeatherFactory wf =
+            WeatherFactory("_apiKey", language: Language.ENGLISH);
+        Weather weather = await wf.currentWeatherByLocation(
+            position.latitude, position.longitude);
+        emit(WeatherBlocSucess(weather));
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
+      // TODO: implement event handler
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_weather/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'My_Weather',
-      theme: ThemeData(
-       
-      ),
+      home: HomeScreen(),
       
     );
   }
